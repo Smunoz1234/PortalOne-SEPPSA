625a4c0e0cc9e3f1f5c777be83e0449c3fd8aad8
Stiven Muñoz Murillo <smunoz@nedugatech.com>
2023-04-12 22:49:11 -0500

    Borrar lineas

--- envio_correo_proveedores.php
+++ envio_correo_proveedores.php
@@ -76,8 +76,87 @@ if ($sw == 1) {
 <title>Pagos efectuados | <?php echo NOMBRE_PORTAL; ?></title>
 <!-- InstanceEndEditable -->
 <!-- InstanceBeginEditable name="head" -->
+
+<script>
+	var json=[];
+	var cant=0;
+
+	function Seleccionar(ID) {
+		var btnBorrarLineas=document.getElementById('btnBorrarLineas');
+
+		var Check = document.getElementById('chkSel'+ID).checked;
+		var sw=-1;
+
+		json.forEach(function(element,index){
+			// console.log(element,index);
+			// console.log(json[index]);
+
+			if(json[index]==ID){
+				sw=index;
+			}
+		});
+
+		if(sw>=0){
+			json.splice(sw, 1);
+			cant--;
+		}else if(Check){
+			json.push(ID);
+			cant++;
+		}
+
+		if(cant>0){
+			$("#btnBorrarLineas").prop('disabled', false);
+		}else{
+			$("#btnBorrarLineas").prop('disabled', true);
+		}
+
+		// console.log(json);
+	}
+
+	function SeleccionarTodos() {
+		var Check = document.getElementById('chkAll').checked;
+		if(Check==false){
+			json=[];
+			cant=0;
+			$("#btnBorrarLineas").prop('disabled', true);
+		}
+		$(".chkSel:not(:disabled)").prop("checked", Check);
+		if(Check){
+			$(".chkSel:not(:disabled)").trigger('change');
+		}
+	}
+
+	function BorrarLineas() {
+		console.log(json);
+
+		// Obtener una referencia a la tabla DataTables
+		let miTabla = $('#miTabla').DataTable();
+
+		// Obtener la filas que se desea eliminar
+		let filasEliminar = [];
+
+		json.forEach(linea => {
+			let fila = miTabla.row(`#line${linea}`);
+			let indice = fila.index();
+
+			// Agregar el indice a la lista
+			filasEliminar.push(indice);
+		});
+
+		// Eliminar las filas de la tabla
+		// console.log(filasEliminar);
+		miTabla.rows(filasEliminar).remove().draw();
+
+		// Restablecer arreglo
+		json = [];
+		cant = 0;
+	}
+</script>
+
 <!-- InstanceEndEditable -->
 </head>
+
+
 
 <body>
 
@@ -182,90 +261,91 @@ if ($sw == 1) {
 		  </div>
          <br>
 
-		 <?php if ($sw == 1) {?>
-          <div class="row">
-           <div class="col-lg-12">
-			    <div class="ibox-content">
+		<?php if ($sw == 1) {?>
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="ibox-content">
+						<div class="table-responsive">
+							<table id="miTabla" class="table table-striped table-bordered table-hover dataTables-example" >
+								<thead>
+									<tr>
+										<th class="text-center form-inline">
+											<div class="checkbox checkbox-success"><input type="checkbox" id="chkAll" value="" onChange="SeleccionarTodos();" title="Seleccionar todos"><label></label></div>
+											<button type="button" id="btnBorrarLineas" title="Borrar lineas" class="btn btn-danger btn-xs" disabled onClick="BorrarLineas();"><i class="fa fa-trash"></i></button>
+										</th>
 
-			<div class="table-responsive">
-                    <table class="table table-striped table-bordered table-hover dataTables-example" >
-                    <thead>
-                     <tr>
-					 	<th class="text-center form-inline">
-							<div class="checkbox checkbox-success"><input type="checkbox" id="chkAll" value="" onChange="SeleccionarTodos();" title="Seleccionar todos"><label></label></div>
-							<button type="button" id="btnBorrarLineas" title="Borrar lineas" class="btn btn-danger btn-xs" disabled onClick="BorrarLinea();"><i class="fa fa-trash"></i></button>
-						</th>
+										<th>Núm. Factura Proveedor</th>
+										<th>Núm. Factura SAP B1</th>
 
-						<th>Núm. Factura Proveedor</th>
-						<th>Núm. Factura SAP B1</th>
+										<th>Fecha Factura</th>
+										<th>Fecha Vencimiento</th>
+										<th>Núm. Pago</th> <!-- Egreso -->
+										<th>Valor Factura</th>
 
-						<th>Fecha Factura</th>
-						<th>Fecha Vencimiento</th>
-						<th>Núm. Pago</th> <!-- Egreso -->
-						<th>Valor Factura</th>
+										<th>Proveedor</th>
 
-						<th>Proveedor</th>
+										<th>Fecha Pago</th>
 
-						<th>Fecha Pago</th>
+										<th>Valor Pago</th>
 
-						<th>Valor Pago</th>
+										<th>Correo electrónico</th>
 
-						<th>Correo electrónico</th>
+										<?php if (false) {?>
+											<th>Estado Envio</th>
+										<?php }?>
+									</tr>
+								</thead>
+								<tbody>
+									<?php $cont = 0;?>
+									<?php while ($row = sqlsrv_fetch_array($SQL)) {?>
 
-						<?php if (false) {?>
-							<th>Estado Envio</th>
-						<?php }?>
-					</tr>
-                    </thead>
-                    <tbody>
-						   <?php while ($row = sqlsrv_fetch_array($SQL)) {?>
-							<tr class="odd gradeX">
-								<td class="text-center form-inline" style="width: 80px;">
-									<div class="checkbox checkbox-success"><input type="checkbox" class="chkSel" id="chkSel<?php //echo $row['LineNum']; ?>" value="" onChange="Seleccionar('<?php //echo $row['LineNum']; ?>');" aria-label="Single checkbox One" <?php //if (($row['LineStatus'] == "C") && ($type == 1)) {echo "disabled='disabled'";}?>><label></label></div>
-									<!--button type="button" class="btn btn-success btn-xs" onClick="ConsultarArticulo('<?php echo base64_encode($row['ItemCode']); ?>');" title="Consultar Articulo"><i class="fa fa-search"></i></button -->
-								</td>
+										<tr class="odd gradeX line" id="line<?php echo $cont; ?>">
+											<td class="text-center form-inline" style="width: 80px;">
+												<div class="checkbox checkbox-success"><input type="checkbox" class="chkSel" id="chkSel<?php echo $cont; ?>" onChange="Seleccionar('<?php echo $cont; ?>');"><label></label></div>
+											</td>
 
-								<td><?php echo $row['numero_factura_Proveedor']; ?></td>
-								<td><?php echo $row['numero_factura_SAPB1']; ?></td>
+											<td><?php echo $row['numero_factura_Proveedor']; ?></td>
+											<td><?php echo $row['numero_factura_SAPB1']; ?></td>
 
-								<td><?php if ($row['fecha_factura'] != "") {echo $row['fecha_factura']->format('Y-m-d');} else {echo "--";}?></td>
-								<td><?php if ($row['fecha_vencimiento_factura'] != "") {echo $row['fecha_vencimiento_factura']->format('Y-m-d');} else {echo "--";}?></td>
-								<td><?php echo $row['numero_pago']; ?></td>
-								<td align="right"><?php echo number_format($row['valor_factura'], 2); ?></td>
+											<td><?php if ($row['fecha_factura'] != "") {echo $row['fecha_factura']->format('Y-m-d');} else {echo "--";}?></td>
+											<td><?php if ($row['fecha_vencimiento_factura'] != "") {echo $row['fecha_vencimiento_factura']->format('Y-m-d');} else {echo "--";}?></td>
+											<td><?php echo $row['numero_pago']; ?></td>
+											<td align="right"><?php echo number_format($row['valor_factura'], 2); ?></td>
 
-								<td><?php echo $row['proveedor']; ?></td>
+											<td><?php echo $row['proveedor']; ?></td>
 
-								<td><?php if ($row['fecha_pago'] != "") {echo $row['fecha_pago']->format('Y-m-d');} else {echo "--";}?></td>
+											<td><?php if ($row['fecha_pago'] != "") {echo $row['fecha_pago']->format('Y-m-d');} else {echo "--";}?></td>
 
-								<td align="right">
-									Efectivo: <?php echo number_format($row['valor_pago_efectivo'], 2); ?>
-									<br>Transferencia: <?php echo number_format($row['valor_pago_tranferencia'], 2); ?>
-									<br>Cheque: <?php echo number_format($row['valor_pago_cheque'], 2); ?>
-									<br>Núm. Cheque: <?php echo $row['numero_cheque']; ?>
-								</td>
+											<td align="right">
+												Efectivo: <?php echo number_format($row['valor_pago_efectivo'], 2); ?>
+												<br>Transferencia: <?php echo number_format($row['valor_pago_tranferencia'], 2); ?>
+												<br>Cheque: <?php echo number_format($row['valor_pago_cheque'], 2); ?>
+												<br>Núm. Cheque: <?php echo $row['numero_cheque']; ?>
+											</td>
 
-								<td>
-									<?php $correos = explode(';', $row['lista_correo_electronico_envio']);?>
-									<?php foreach ($correos as &$correo) {?>
-										<?php echo "<br>" . $correo; ?>
+											<td>
+												<?php $correos = explode(';', $row['lista_correo_electronico_envio']);?>
+												<?php foreach ($correos as &$correo) {?>
+													<?php echo "<br>" . $correo; ?>
+												<?php }?>
+											</td>
+
+											<?php if (false) {?>
+												<td>
+													<br>Estado: <?php echo $row['estado_envio_correo']; ?>
+													<br>Mensaje: <?php echo $row['mensaje_envio']; ?>
+												</td>
+											<?php }?>
+										</tr>
+
+									<?php $cont++;?>
 									<?php }?>
-								</td>
-
-								<?php if (false) {?>
-									<td>
-										<br>Estado: <?php echo $row['estado_envio_correo']; ?>
-										<br>Mensaje: <?php echo $row['mensaje_envio']; ?>
-									</td>
-								<?php }?>
-							</tr>
-							<?php }?>
-						</tbody>
-                    </table>
-              </div>
-			</div> <!-- table-responsive -->
-
-			 </div>
-          </div>
+								</tbody>
+							</table>
+						</div> <!-- table-responsive -->
+					</div> <!-- ibox-content -->
+				</div> <!-- col-lg-12 -->
+          	</div> <!-- row -->
 		<?php }?>
 
 		</div>
