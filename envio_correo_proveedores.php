fe539d68b9f3653606fb7c8b2febc9b65f870da9
Stiven Muñoz Murillo <smunoz@nedugatech.com>
2023-04-12 21:38:10 -0500

    ajustando esta vaina

--- envio_correo_proveedores.php
+++ envio_correo_proveedores.php
@@ -22,14 +22,14 @@ if (isset($_GET['FI_Registro']) && $_GET['FI_Registro'] != "") {
     $nuevaFecha = date('Y-m-d', $nuevaFecha);
 
     // SMM, 11/04/2023
-    $FI_Registro = $nuevaFecha;
+    // $FI_Registro = $nuevaFecha;
 }
 if (isset($_GET['FF_Registro']) && $_GET['FF_Registro'] != "") {
     $FF_Registro = $_GET['FF_Registro'];
     $sw = 1;
 } else {
     // SMM, 11/04/2023
-    $FF_Registro = date('Y-m-d');
+    // $FF_Registro = date('Y-m-d');
 }
 
 // AGREGAR FILTROS DE FECHA DE PAGO
@@ -44,14 +44,14 @@ if (isset($_GET['FI_Pago']) && $_GET['FI_Pago'] != "") {
     $nuevaFecha = date('Y-m-d', $nuevaFecha);
 
     // SMM, 11/04/2023
-    // $FI_Pago = $nuevaFecha;
+    $FI_Pago = $nuevaFecha;
 }
 if (isset($_GET['FF_Pago']) && $_GET['FF_Pago'] != "") {
     $FF_Pago = $_GET['FF_Pago'];
     $sw = 1;
 } else {
     // SMM, 11/04/2023
-    // $FF_Pago = date('Y-m-d');
+    $FF_Pago = date('Y-m-d');
 }
 
 if ($sw == 1) {
@@ -118,24 +118,6 @@ if ($sw == 1) {
 							</div>
 
 							<div class="form-group">
-								<label class="col-lg-1 control-label">Proveedor</label>
-								<div class="col-lg-3">
-									<input name="Cliente" type="hidden" id="Cliente" value="<?php if (isset($_GET['Cliente']) && ($_GET['Cliente'] != "")) {echo $_GET['Cliente'];}?>">
-									<input name="NombreCliente" type="text" class="form-control" id="NombreCliente" placeholder="Para TODOS, dejar vacio..." value="<?php if (isset($_GET['NombreCliente']) && ($_GET['NombreCliente'] != "")) {echo $_GET['NombreCliente'];}?>">
-								</div>
-
-
-								<label class="col-lg-1 control-label">Fecha de registro</label>
-								<div class="col-lg-3">
-									<div class="input-daterange input-group" id="datepicker">
-										<input name="FI_Registro" type="text" class="input-sm form-control fecha" id="FI_Registro" placeholder="Fecha inicial" value="<?php echo $FI_Registro; ?>" autocomplete="off"/>
-										<span class="input-group-addon">hasta</span>
-										<input name="FF_Registro" type="text" class="input-sm form-control fecha" id="FF_Registro" placeholder="Fecha final" value="<?php echo $FF_Registro; ?>" autocomplete="off" />
-									</div>
-								</div>
-							</div>
-
-							<div class="form-group">
 								<label class="col-lg-1 control-label">Fecha de pago</label>
 								<div class="col-lg-3">
 									<div class="input-daterange input-group" id="datepicker">
@@ -149,21 +131,48 @@ if ($sw == 1) {
 								<div class="col-lg-3">
 									<input name="CiudadSede" type="text" class="form-control" id="CiudadSede" maxlength="100" value="<?php if (isset($_GET['CiudadSede']) && ($_GET['CiudadSede'] != "")) {echo $_GET['CiudadSede'];}?>">
 								</div>
+							</div>
+
+							<div class="form-group">
+								<label class="col-lg-1 control-label">Proveedor</label>
+								<div class="col-lg-3">
+									<input name="Cliente" type="hidden" id="Cliente" value="<?php if (isset($_GET['Cliente']) && ($_GET['Cliente'] != "")) {echo $_GET['Cliente'];}?>">
+									<input name="NombreCliente" type="text" class="form-control" id="NombreCliente" placeholder="Para TODOS, dejar vacio..." value="<?php if (isset($_GET['NombreCliente']) && ($_GET['NombreCliente'] != "")) {echo $_GET['NombreCliente'];}?>">
+								</div>
 
 								<div class="col-lg-4">
 									<div class="btn-group pull-right">
-										<button type="submit" class="btn btn-outline btn-success" style="margin-right: 10px;"><i class="fa fa-search"></i> Buscar</button>
-										<button type="button" class="btn btn-outline btn-warning" ><i class="fa fa-save"></i> Guardar</button>
+										<button type="submit" class="btn btn-outline btn-success"><i class="fa fa-search"></i> Buscar</button>
 									</div>
 								</div>
 							</div>
 
 							<?php if (($sw == 1) && sqlsrv_has_rows($SQL)) {?>
 								<div class="form-group">
-									<div class="col-lg-10">
-										<a href="exportar_excel.php?exp=10&Cons=<?php echo base64_encode(implode(",", $Param)); ?>&sp=<?php echo base64_encode($sp); ?>">
-											<img src="css/exp_excel.png" width="50" height="30" alt="Exportar a Excel" title="Exportar a Excel"/>
-										</a>
+									<label class="col-xs-12"><h3 class="bg-success p-xs b-r-sm"><i class="fa fa-envelope"></i> Información para envio de correos</h3></label>
+								</div>
+
+								<div class="form-group">
+									<label class="col-lg-1 control-label">ID</label>
+									<div class="col-lg-3">
+										<input name="ID" type="text" class="form-control" id="ID" value="<?php echo $_GET['ID'] ?? ""; ?>" readonly>
+									</div>
+
+									<div class="col-lg-4"></div>
+
+									<div class="col-lg-4">
+										<button type="button" class="btn btn-outline btn-warning"><i class="fa fa-save"></i> Guardar</button>
+									</div>
+								</div>
+
+								<div class="form-group">
+									<label class="col-lg-1 control-label">Descripción</label>
+									<div class="col-lg-7">
+										<textarea name="descripcion" id="descripcion" rows="5" cols="100" maxlength="250"></textarea>
+									</div>
+
+									<div class="col-lg-4">
+										<a class="btn btn-outline btn-info" href="exportar_excel.php?exp=10&Cons=<?php echo base64_encode(implode(",", $Param)); ?>&sp=<?php echo base64_encode($sp); ?>">Exportar a Excel</a>
 									</div>
 								</div>
 							<?php }?>
@@ -177,45 +186,84 @@ if ($sw == 1) {
           <div class="row">
            <div class="col-lg-12">
 			    <div class="ibox-content">
+
 			<div class="table-responsive">
                     <table class="table table-striped table-bordered table-hover dataTables-example" >
                     <thead>
                      <tr>
-						<th>Factura proveedor</th>
-						<th>Núm. Interno de factura</th>
-						<th>Fecha factura</th>
-						<th>Fecha vencimiento</th>
-						<th>Núm. de pago</th>
-						<th>Valor factura</th>
-						<th>Valor pagado</th>
-						<th>Fecha pago</th>
-						<th>Efectivo</th>
-						<th>Transferencia</th>
-						<th>Cheque</th>
-						<th>Núm. Cheque</th>
+					 	<th class="text-center form-inline">
+							<div class="checkbox checkbox-success"><input type="checkbox" id="chkAll" value="" onChange="SeleccionarTodos();" title="Seleccionar todos"><label></label></div>
+							<button type="button" id="btnBorrarLineas" title="Borrar lineas" class="btn btn-danger btn-xs" disabled onClick="BorrarLinea();"><i class="fa fa-trash"></i></button>
+						</th>
+
+						<th>Núm. Factura Proveedor</th>
+						<th>Núm. Factura SAP B1</th>
+
+						<th>Fecha Factura</th>
+						<th>Fecha Vencimiento</th>
+						<th>Núm. Pago</th> <!-- Egreso -->
+						<th>Valor Factura</th>
+
+						<th>Proveedor</th>
+
+						<th>Fecha Pago</th>
+
+						<th>Valor Pago</th>
+
+						<th>Correo electrónico</th>
+
+						<?php if (false) {?>
+							<th>Estado Envio</th>
+						<?php }?>
 					</tr>
                     </thead>
                     <tbody>
 						   <?php while ($row = sqlsrv_fetch_array($SQL)) {?>
 							<tr class="odd gradeX">
-								<td><?php if ($row['FacturaProveedor'] != "") {?><a href="prov_detalle_facturas_proveedores.php?id=<?php echo base64_encode($row['DocEntryFactura']); ?>&return=<?php echo base64_encode($_SERVER['QUERY_STRING']); ?>&pag=<?php echo base64_encode('prov_pagos_efectuados.php'); ?>"><?php echo $row['FacturaProveedor']; ?></a><?php } else {echo "--";}?></td>
-								<td><?php echo $row['DocNumFactura']; ?></td>
-								<td><?php if ($row['FechaContFactura'] != "") {echo $row['FechaContFactura']->format('Y-m-d');} else {echo "--";}?></td>
-								<td><?php if ($row['FechaVencFactura'] != "") {echo $row['FechaVencFactura']->format('Y-m-d');} else {echo "--";}?></td>
-								<td><?php echo $row['NumPagoEfectuado']; ?></td>
-								<td align="right"><?php echo number_format($row['DocTotal'], 2); ?></td>
-								<td align="right"><?php echo number_format($row['ValorPago'], 2); ?></td>
-								<td><?php if ($row['FechaPago'] != "") {echo $row['FechaPago']->format('Y-m-d');} else {echo "--";}?></td>
-								<td align="right"><?php echo number_format($row['CashSum'], 2); ?></td>
-								<td align="right"><?php echo number_format($row['TrsfrSum'], 2); ?></td>
-								<td align="right"><?php echo number_format($row['CheckSum'], 2); ?></td>
-								<td align="right"><?php echo $row['CheckNum']; ?></td>
+								<td class="text-center form-inline" style="width: 80px;">
+									<div class="checkbox checkbox-success"><input type="checkbox" class="chkSel" id="chkSel<?php //echo $row['LineNum']; ?>" value="" onChange="Seleccionar('<?php //echo $row['LineNum']; ?>');" aria-label="Single checkbox One" <?php //if (($row['LineStatus'] == "C") && ($type == 1)) {echo "disabled='disabled'";}?>><label></label></div>
+									<!--button type="button" class="btn btn-success btn-xs" onClick="ConsultarArticulo('<?php echo base64_encode($row['ItemCode']); ?>');" title="Consultar Articulo"><i class="fa fa-search"></i></button -->
+								</td>
+
+								<td><?php echo $row['numero_factura_Proveedor']; ?></td>
+								<td><?php echo $row['numero_factura_SAPB1']; ?></td>
+
+								<td><?php if ($row['fecha_factura'] != "") {echo $row['fecha_factura']->format('Y-m-d');} else {echo "--";}?></td>
+								<td><?php if ($row['fecha_vencimiento_factura'] != "") {echo $row['fecha_vencimiento_factura']->format('Y-m-d');} else {echo "--";}?></td>
+								<td><?php echo $row['numero_pago']; ?></td>
+								<td align="right"><?php echo number_format($row['valor_factura'], 2); ?></td>
+
+								<td><?php echo $row['proveedor']; ?></td>
+
+								<td><?php if ($row['fecha_pago'] != "") {echo $row['fecha_pago']->format('Y-m-d');} else {echo "--";}?></td>
+
+								<td align="right">
+									Efectivo: <?php echo number_format($row['valor_pago_efectivo'], 2); ?>
+									<br>Transferencia: <?php echo number_format($row['valor_pago_tranferencia'], 2); ?>
+									<br>Cheque: <?php echo number_format($row['valor_pago_cheque'], 2); ?>
+									<br>Núm. Cheque: <?php echo $row['numero_cheque']; ?>
+								</td>
+
+								<td>
+									<?php $correos = explode(';', $row['lista_correo_electronico_envio']);?>
+									<?php foreach ($correos as &$correo) {?>
+										<?php echo "<br>" . $correo; ?>
+									<?php }?>
+								</td>
+
+								<?php if (false) {?>
+									<td>
+										<br>Estado: <?php echo $row['estado_envio_correo']; ?>
+										<br>Mensaje: <?php echo $row['mensaje_envio']; ?>
+									</td>
+								<?php }?>
 							</tr>
 							<?php }?>
 						</tbody>
                     </table>
               </div>
-			</div>
+			</div> <!-- table-responsive -->
+
 			 </div>
           </div>
 		<?php }?>
